e6459707e92fb6ce91284d23f6d75468fef89adb
benxta4424 <benxtaben@gmail.com>
2024-05-04 21:48:54 +0300

    Creare joc Piatra-Hartie-Foarfeca cu ajutorul functiilor
    Punem utilizatorul sa aleaga una din cele 3 variante(P-H-F)
    Generam o alegere aleatoare cu ajutorul functiei predefinite Math.Random()
    Comparam cele 2 alegeri prin intermediul unui switch
    In caz pozitiv,incrementam scorul utilizatorului,in caz negativ,viceversa
    Egalul nu este luat in considerare
    Facem un loop cu cele 2 incrementari si care ajunge prima la 3 decide soarta meciului
    La 3 pentru ca sunt 5 runde

---
+++ java.js
@@ -0,0 +1,117 @@
+
+
+function getHumanChoice()
+{
+    let humanChoice=prompt("Piatra,Hartie sau Foarfeca?")
+    return humanChoice
+}
+
+function getComputerChoice()
+{
+    let computerChoice=Math.floor(Math.random()*3)+1
+    switch(computerChoice)
+    {
+        case 1:
+            computerChoice='Piatra'
+            break
+        case 2:
+            computerChoice='Hartie'
+            break
+        case 3:
+            computerChoice='Foarfeca'
+            break
+    }
+    return computerChoice
+}
+function playRound(hChoice,cChoice)
+{
+    let humanScore=0,computerScore=0
+    switch(hChoice)
+    {
+        case 'Piatra':
+            if(cChoice==='Hartie')
+            {
+                alert(`Ai pierdut! ${cChoice} bate ${hChoice} !`)
+                computerScore++
+            }
+            else if(cChoice==='Foarfeca')
+            {
+                alert(`Ai castigat! ${hChoice} bate ${cChoice} !`)
+                humanScore++;
+            }
+            else
+            {
+                alert('Egal!')
+                humanScore=0
+                computerScore=0
+            }
+            break
+        case 'Hartie':
+            if(cChoice==='Foarfeca')
+            {
+                alert(`Ai pierdut! ${cChoice} bate ${hChoice} !`)
+                computerScore++
+            }
+            else if(cChoice==='Piatra')
+            {
+                alert(`Ai castigat! ${hChoice} bate ${cChoice} !`)
+                humanScore++;
+            }
+            else
+            {
+                alert('Egal!')
+                humanScore=0
+                computerScore=0
+            }
+            break
+        case 'Foarfeca':
+            if(cChoice==='Piatra')
+            {
+                alert(`Ai pierdut! ${cChoice} bate ${hChoice} !`)
+                computerScore++
+            }
+            else if(cChoice==='Hartie')
+            {
+                alert(`Ai castigat! ${hChoice} bate ${cChoice} !`)
+                humanScore++;
+            }
+            else
+            {
+                alert('Egal!')
+                humanScore=0
+                computerScore=0
+            }
+            break
+    }
+    if(humanScore==1)
+    return 'scorOM'
+    else if(computerScore==1)
+    return 'scorCOMPUTER'
+    else
+    return 
+}
+
+let ver,hsc=0,csc=0,i
+    do{
+    const humanSelection=getHumanChoice()
+    const computerSelection=getComputerChoice()
+    ver=playRound(humanSelection,computerSelection)
+    if(ver=='scorOM')
+    hsc++
+    else if(ver=='scorCOMPUTER')
+    csc++
+
+
+    if(hsc==3)
+     {
+         alert('Ai castigat meciul!')
+         break
+     }
+     else if(csc==3)
+     {
+        alert('Ai pierdut meciul!')
+        break
+     }
+    }
+    while(hsc!=3||csc!=3)
+
