7b3204bbb89364d0870914a8e610aa667ea37a97
benxta4424 <podean.beni@gmail.com>
2024-05-27 19:11:04 +0100

    Am creat jocul PHF cu ajutorul eventurilor
    Creerea unei UI pentru a face jocul mai intuitiv
    Creerea de eventListners pentru alegerea omului,alegerea calculatorului fiind random
    Cu ajutorul functiilor s a creat fiecare functionalitate in parte
    Practic am construit codul mare din mai multe coduri mici
    
    P. S. am reinceput dupa 3 saptamani pentru ca mi s a ars placa de baza la laptopul vechi si n am avut pe ce sa fac :(
        Mi a fost foarte dor sincer <3

--- java.js
+++ java.js
@@ -1,114 +1,121 @@
-
-
-
-function getComputerChoice()
+scorOm=0
+scorCalc=0
+scorEg=0
+function getCompChoice()
 {
-    let computerChoice=Math.floor(Math.random()*3)+1
-    switch(computerChoice)
+    let rezultat=Math.floor(Math.random()*3)+1
+    switch(rezultat)
     {
         case 1:
-            computerChoice='Piatra'
-            break
-        case 2:
-            computerChoice='Hartie'
-            break
-        case 3:
-            computerChoice='Foarfeca'
-            break
-    }
-    return computerChoice
-}
-function playRound(hChoice,cChoice)
-{
-    let humanScore=0,computerScore=0
-    switch(hChoice)
-    {
-        case 'Piatra':
-            if(cChoice==='Hartie')
-            {
-                alert(`Ai pierdut! ${cChoice} bate ${hChoice} !`)
-                computerScore++
-            }
-            else if(cChoice==='Foarfeca')
-            {
-                alert(`Ai castigat! ${hChoice} bate ${cChoice} !`)
-                humanScore++;
-            }
-            else
-            {
-                alert('Egal!')
-                humanScore=0
-                computerScore=0
-            }
-            break
-        case 'Hartie':
-            if(cChoice==='Foarfeca')
-            {
-                alert(`Ai pierdut! ${cChoice} bate ${hChoice} !`)
-                computerScore++
-            }
-            else if(cChoice==='Piatra')
-            {
-                alert(`Ai castigat! ${hChoice} bate ${cChoice} !`)
-                humanScore++;
-            }
-            else
-            {
-                alert('Egal!')
-                humanScore=0
-                computerScore=0
-            }
-            break
-        case 'Foarfeca':
-            if(cChoice==='Piatra')
-            {
-                alert(`Ai pierdut! ${cChoice} bate ${hChoice} !`)
-                computerScore++
-            }
-            else if(cChoice==='Hartie')
-            {
-                alert(`Ai castigat! ${hChoice} bate ${cChoice} !`)
-                humanScore++;
-            }
-            else
-            {
-                alert('Egal!')
-                humanScore=0
-                computerScore=0
-            }
-            break
-    }
-    if(humanScore==1)
-    return 'scorOM'
-    else if(computerScore==1)
-    return 'scorCOMPUTER'
-    else
-    return 
-}
-function playGame()
-{
-let ver,hsc=0,csc=0
-    do{
-    const humanSelection=getHumanChoice()
-    const computerSelection=getComputerChoice()
-    ver=playRound(humanSelection,computerSelection)
-    if(ver=='scorOM')
-    hsc++
-    else if(ver=='scorCOMPUTER')
-    csc++
-
-
-    if(hsc==3)
-     {
-         alert('Ai castigat meciul!')
-         break
-     }
-     else if(csc==3)
-     {
-        alert('Ai pierdut meciul!')
+            if(rezultat===1)
+                rezultat='Piatra'
         break
-     }
+
+        case 2: 
+            if(rezultat===2)
+                rezultat='Hartie'
+        break
+
+        case 3:
+            if(rezultat===3)
+                rezultat='Foarfeca'
+        break
     }
-    while(hsc!=3||csc!=3)
+
+    return rezultat
 }
-playGame()
+
+
+
+function validare()
+{
+    
+    
+    const piatra=document.querySelector('.piatra')
+    const hartie=document.querySelector('.hartie')
+    const foarfeca=document.querySelector('.foarfeca')
+
+    piatra.addEventListener("click",()=>
+    {
+        let cChoice=getCompChoice()
+        if(cChoice==='Hartie')
+            scorCalc++
+        else if(cChoice==='Foarfeca')
+            scorOm++
+        else
+            if(cChoice==='Piatra')
+                scorEg++
+
+            updateScoruri()
+            
+    })
+
+    hartie.addEventListener("click",()=>
+    {
+        let cChoice=getCompChoice()
+        if(cChoice==='Foarfeca')
+            scorCalc++
+        else if(cChoice==='Piatra')
+            scorOm++
+        else if(cChoice==='Hartie')
+            scorEg++
+
+        updateScoruri()
+        
+    })
+
+    foarfeca.addEventListener("click",()=>
+    {
+        let cChoice=getCompChoice()
+        if(cChoice==='Piatra')
+            scorCalc++
+        else if(cChoice==='Hartie')
+            scorOm++
+        else if(cChoice==='Foarfeca')
+            scorEg++
+
+        updateScoruri()
+        
+    })
+    
+
+    
+
+}
+
+function updateScoruri()
+{
+    document.getElementById('om').innerText=scorOm
+    document.getElementById('computer').innerText=scorCalc
+    document.getElementById('scorEgal').innerText=scorEg
+}
+
+function resetGame()
+{
+    scorCalc=0
+    scorOm=0
+    scorEg=0
+    updateScoruri()
+
+}
+
+function verificaWIn()
+{
+    if(scorOm>=5)
+        {
+            const winner=document.querySelector('.winner')
+            winner.textContent='Ai castigat!'
+        }
+    else if(scorCalc>=5)
+        {
+            const winner=document.querySelector('.winner')
+            winner.textContent='AI pierdut!'
+        }
+}
+function play()
+{
+   validare()
+   verificaWIn()
+}
+
+play()
